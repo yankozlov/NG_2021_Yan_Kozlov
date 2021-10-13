4f97e96218c42456fbabd004dfd4a4f3241d45dc
yankozlov <kozlov.yan1@gmail.com>
2021-10-13 16:45:19 +0300

    upgraded task 3

--- lesson_1/task_3/main.cpp
+++ lesson_1/task_3/main.cpp
@@ -6,9 +6,16 @@ int main()
 {
     int a = 50;
     int b = 20;
+    //variant #1
     a += b;
-    b = a - b;
+    b = a-b;
     a -= b;
+    //variant #2
+    a *= b;
+    b = a/b;
+    a /= b;
+    //variant #3
+    swap (a, b);
     cout << "a = " << a << endl;
     cout << "b = " << b << endl;
 }
